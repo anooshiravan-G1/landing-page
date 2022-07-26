efb5905ffbf587e1d8c919e20f935c03a4f03a67
anooshiravan-G1 <68273221+anooshiravan-G1@users.noreply.github.com>
2022-07-26 14:14:25 +0430

    heroku 4

--- landingPage/settings.py
+++ landingPage/settings.py
@@ -134,10 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-DISABLE_COLLECTSTATIC=1
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
